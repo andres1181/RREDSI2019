fb60c28a376ee5b2456622be1353879aadfd743d
andres1181 <andreslozano1118@gmail.com>
2019-09-22 13:46:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rredsi2019/Panel_Consulta.java
@@ -0,0 +1,14 @@
+package com.example.rredsi2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Panel_Consulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel__consulta);
+    }
+}
